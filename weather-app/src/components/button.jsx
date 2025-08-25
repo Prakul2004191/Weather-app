f527b691fa309e7f39668c4149bd8c32cafed039
Prakul2004191 <Prakulgupta2004@gmail.com>
2025-08-25 11:09:14 +0530

    Update button.jsx

--- weather-app/src/components/button.jsx
+++ weather-app/src/components/button.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
-const Button =(props) => {
-    return (
-        <button className="button" onClick={props.onClick}>
-            {props.value}
-        </button>
-    )
-}
+const Button = (props) => {
+  // handle Enter key press
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      props.onClick();
+    }
+  };
+
+  return (
+    <button
+      className="button"
+      onClick={props.onClick}
+      onKeyDown={handleKeyDown}   // 👈 added here
+    >
+      {props.value}
+    </button>
+  );
+};
 
 export default Button;
